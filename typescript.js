46909dc4a220505c1dd1a26763e452bb64386bc6
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-01-17 16:05:24 +0700

    fix(typescript): disable typescript error suppression
    These rules don't make sense, because typescript errors can only be suppressed on purpose.

--- typescript.js
+++ typescript.js
@@ -8,9 +8,11 @@ module.exports = {
         '@typescript-eslint'
     ],
     rules: {
+        '@typescript-eslint/ban-ts-comment': 'warn',
         '@typescript-eslint/consistent-type-definitions': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off'
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/prefer-ts-expect-error': 'off'
     }
 };
